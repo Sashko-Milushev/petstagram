f79939a1b6133325ac2722a8db808468c5d153d5
Sashko Milushev <milushev.sashko@gmail.com>
2022-09-29 17:33:38 +0300

    Implement base html and make all pages extends it. Make correct paths for static files

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -113,9 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'staticfiles/static', )
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 
 
 # Default primary key field type
