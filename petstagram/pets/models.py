3081365c921816f1b1a300f932e172723faa2ffa
Sashko Milushev <milushev.sashko@gmail.com>
2022-10-11 20:50:04 +0300

    Implement Photos model

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -33,3 +33,6 @@ class Pet(models.Model):
             self.slug = slugify(f'{self.id}-{self.name}')
 
         return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'ID={self.id} Name={self.name}'
---
+++ petstagram/photos/validators.py
@@ -0,0 +1,9 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_image(image):
+    filesize = image.file.size
+    megabyte_limit = 5.0
+    if filesize > megabyte_limit * 1024 * 1024:
+        raise ValidationError(f'Max file size is {megabyte_limit}MB!')
+
