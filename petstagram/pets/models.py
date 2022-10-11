2f72c377732457b2e597124b3f9b59dae9cc9bb4
Sashko Milushev <milushev.sashko@gmail.com>
2022-10-11 19:07:36 +0300

    Make first model and connect db

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    MAX_NAME = 30
+    name = models.CharField(
+        max_length=MAX_NAME,
+        null=False,
+        blank=False,
+    )
+
+    personal_photo = models.URLField(
+        null=False,
+        blank=False,
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
