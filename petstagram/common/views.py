2c5f7c8352559d381e4ac98acf1e040b4c6b9ea4
Sashko Milushev <milushev.sashko@gmail.com>
2022-10-12 20:23:59 +0300

    Implement like button view

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.common.models import PhotoLike
 from petstagram.photos.models import Photo
 
 
@@ -18,8 +19,38 @@ def index(request):
     photos = [apply_likes_count(photo) for photo in Photo.objects.all()]
     photos = [apply_user_liked_photo(photo) for photo in photos]
 
-
     context = {
         'photos': photos,
     }
     return render(request, 'common/home-page.html', context)
+
+
+def get_user_liked_photos(photo_id):
+    # TODO: fix when have auth
+    return PhotoLike.objects.filter(photo_id=photo_id)
+
+
+def like_photo(request, photo_id):
+
+    user_liked_photos = get_user_liked_photos(photo_id)
+
+    if user_liked_photos:
+        user_liked_photos.delete()
+    else:
+        PhotoLike.objects.create(
+            photo_id=photo_id,
+        )
+
+    # Variant 2
+
+
+    redirect_path = request.META['HTTP_REFERER'] + f'#photo-{photo_id}'
+
+    return redirect(redirect_path)
+
+    # Variant 1 (could be used for update!)
+    # photo_like = PhotoLike(
+    #     photo_id=photo_id,
+    # )
+    # photo_like.save()
+
