c75e60260cf91b1e1a14b474fe9a03178b6534dd
Sashko Milushev <milushev.sashko@gmail.com>
2022-11-15 17:26:07 +0200

    Make auth model 'AppUser' and add it in settings.py

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,3 +1,52 @@
+from enum import Enum
+
+from django.contrib.auth import models as auth_models
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class ChoicesEnumMixin:
+    @classmethod
+    def choices(cls):
+        return [(x.name, x.value) for x in cls]
+
+    @classmethod
+    def max_len(cls):
+        return max(len(name) for name, _ in cls.choices())
+
+
+class Gender(ChoicesEnumMixin, Enum):
+    male = "Male"
+    female = "Female"
+    DoNotShow = 'Do not show'
+
+
+class AppUser(auth_models.AbstractUser):
+    MIN_LENGTH_FIRST_NAME = 2
+    MAX_LENGTH_FIRST_NAME = 30
+
+    MIN_LENGTH_LAST_NAME = 2
+    MAX_LENGTH_LAST_NAME = 30
+
+    first_name = models.CharField(
+        max_length=MAX_LENGTH_FIRST_NAME,
+        validators=(
+            validators.MinLengthValidator(MIN_LENGTH_FIRST_NAME),
+        ),
+    )
+
+    last_name = models.CharField(
+        max_length=MAX_LENGTH_LAST_NAME,
+        validators=(
+            validators.MinLengthValidator(MAX_LENGTH_LAST_NAME),
+        ),
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    gender = models.CharField(
+        choices=Gender.choices(),
+        max_length=Gender.max_len(),
+    )
